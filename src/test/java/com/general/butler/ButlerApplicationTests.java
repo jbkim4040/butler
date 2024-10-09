6beacec4348215bc591b5ea17c61b86026c9c0e2
jbkim4040 <jbkim404037@gmail.com>
2024-10-10 00:10:21 +0900

    The Great Beginning

---
+++ src/test/java/com/general/butler/ButlerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.general.butler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ButlerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
